fe166211da5f71c08e8b191a8831d514a4d18039
AbdullahPro2 <zeb16208@gmail.com>
2024-04-21 21:48:45 +0200

    file structure set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 function App() {
+  const [data, setData] = useState(false);
+
   return (
     <div className=" bg-red-500  text-center text-lg font-extrabold">
       Hello Mulhouse Travail
