2cb5c2145e25649b45555096b5ecb1437cc13f5b
AbdullahPro2 <zeb16208@gmail.com>
2024-04-24 00:14:49 +0200

    bug fixed

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../../assets/mainLogos/mulhouseTravailLogo.png';
-
+import logo from '../../assets/mainLogos/mulhouseTravailLogo.svg';
 function Logo() {
   return (
     <NavLink to="/Mulhouse-Travail/">
-      <img src={logo} alt="Main Logo" className="w-20" />
+      <img src={logo} alt="Main Logo" className="w-52" />
     </NavLink>
   );
 }
