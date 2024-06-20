4907e11b7f8cc92d2d7f43f0d75deb8fdafc5dd6
AbdullahPro2 <zeb16208@gmail.com>
2024-06-20 23:43:50 +0200

    bug fixed in contac us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -77,6 +77,7 @@ function Contact() {
               name="redirect"
               value="https://mulhouse-travail.netlify.app/mercipourcontactez"
             />
+
             <button
               type="submit"
               className="send-message mt-4 w-full cursor-pointer rounded border-0 bg-[#1b64ac] px-4 py-2 text-center font-normal leading-loose text-teal-50 md:text-lg lg:mt-16"
